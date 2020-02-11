bb286852277d3f1a91427a0786a7d4629479f497
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2020-02-11 11:57:51 +0100

    added express.static to serve static files

--- expressrouter.js
+++ expressrouter.js
@@ -3,8 +3,11 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res)=>{
-    res.send('Heloo world!');
+    
+    // res.send('./index.html');
+    
 })
+app.use(express.static('public'));
 
 app.listen(port, (req, res)=>{
     console.log(`Server running on port: ${port}`);
